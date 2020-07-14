c8e6f0ded274218d6730af6e68356533b4202362
Your Name <you@example.com>
2020-07-13 21:58:13 -0300

    Estrutura inicial do projeto Spring Boot

---
+++ src/main/java/br/com/carlos/restapijparabbitmq/RestapiJpaRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package br.com.carlos.restapijparabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiJpaRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiJpaRabbitmqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/carlos/restapijparabbitmq/RestapiJpaRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carlos.restapijparabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiJpaRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
